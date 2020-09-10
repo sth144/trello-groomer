d84f1503866adc0b076ac344bee03e370ca3acf2
sth144 <sthinds144@gmail.com>
2020-09-10 16:42:22 -0500

    Add initial functionality (due date assignments and list movements) to work groomer

--- src/groomer/work.groomer.ts
+++ src/groomer/work.groomer.ts
@@ -2,14 +2,21 @@ import { BoardModel } from "../model/board.model";
 import { List } from "../lib/list.interface";
 import { BoardController } from "../controller/board.controller";
 import { logger } from "../lib/logger";
+import { 
+    cardIsComplete, cardDueToday, cardDueThisWeek, 
+    cardHasDueDate, wasMovedFromToListFilterFactory, Not 
+} from "../lib/card.filters";
+import { join } from "path";
+import { existsSync } from "fs";
+import { parseAutoDueConfig } from "../lib/parse.utils";
+const secrets = require("../../config/key.json");
+const boards = require("../../config/boards.json");
 
 export class WorkBoardModel extends BoardModel {
     lists: {
         inbox: List;
         backlog: List;
-        month: List;
         week: List;
-        tomorrow: List;
         day: List;
         done: List;
         pinned: List;
@@ -17,9 +24,7 @@ export class WorkBoardModel extends BoardModel {
     } = {
         inbox: new List(),
         backlog: new List(),
-        month: new List(),
         week: new List(),
-        tomorrow: new List(),
         day: new List(),
         done: new List(),
         pinned: new List(),
@@ -42,12 +47,79 @@ export const WorkGroomer = function() {
         start = new Date();
         logger.info("Started " + start.toString());
 
+        logger.info("Building model")
+        workModel = new WorkBoardModel(boards.work.id);
+
+        logger.info("Initializing controllers");
+        workController = new BoardController<WorkBoardModel>(workModel, {
+            key: secrets.key,
+            token: secrets.token
+        });
+
+        await workController.wakeUp();
     };
 
     const groom = async () => {
         logger.info("Grooming (Work)");
 
         delete require.cache;
+
+        /** assign due dates to cards without due dates */
+        const autoDueConfigPath = join(process.cwd(), "config/auto-due.config.json");
+        if (existsSync(autoDueConfigPath)) {
+            const autoDueConfig = parseAutoDueConfig(autoDueConfigPath) as { [s: string]: number };
+
+            logger.info("Updating due dates based on manual list movements");
+
+            await workController.assignDueDatesIf(workModel.lists.backlog.id, autoDueConfig.backlog,
+                wasMovedFromToListFilterFactory(workModel.lists.backlog.id, [
+                    workModel.lists.week.id,
+                    workModel.lists.day.id
+                ]), 7 /** one week of random stagger (unique on per card basis, not per call to assignDueDatesIf */);
+            await workController.assignDueDatesIf(workModel.lists.week.id, autoDueConfig.week,
+                wasMovedFromToListFilterFactory(workModel.lists.week.id, [
+                    workModel.lists.day.id
+                ]));
+
+            await workController.assignDueDatesIf(workModel.lists.day.id, autoDueConfig.day, 
+                Not(cardHasDueDate));
+            await workController.assignDueDatesIf(workModel.lists.week.id, autoDueConfig.week,
+                Not(cardHasDueDate)); 
+            await workController.assignDueDatesIf(workModel.lists.backlog.id, autoDueConfig.backlog, 
+                Not(cardHasDueDate));
+
+        }
+
+        logger.info("Updating list placements");
+
+        await workController.moveCardsFromToIf([
+            workModel.lists.inbox.id,
+            workModel.lists.backlog.id,
+            workModel.lists.week.id,
+            workModel.lists.day.id
+        ], workModel.lists.done.id, cardIsComplete);
+
+        /** move cards due today to Today */
+        await workController.moveCardsFromToIf([
+            workModel.lists.inbox.id,
+            workModel.lists.backlog.id,
+            workModel.lists.week.id,
+        ], workModel.lists.day.id, cardDueToday);
+
+        /** move cards due this week to Week */
+        await workController.moveCardsFromToIf([
+            workModel.lists.inbox.id,
+            workModel.lists.backlog.id,
+        ], workModel.lists.week.id, cardDueThisWeek);
+
+        /** move all cards in inbox with due date to backlog */
+        await workController.moveCardsFromToIf([
+            workModel.lists.inbox.id
+        ], workModel.lists.backlog.id, cardHasDueDate);
+
+        logger.info("Marking appropriate items done");
+
+        await workController.markCardsInListDone(workModel.lists.done.id);
     }
 
     return {
